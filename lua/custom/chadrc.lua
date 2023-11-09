a042e888ed3b3caef25114b7400d389ed9b0318c
Antonin Bonte <antonin.bonte@quickfds.eu>
2023-11-09 21:53:29 +0100

    added back ()

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
   theme = "onedark",
@@ -12,9 +12,10 @@ M.ui = {
   hl_add = highlights.add,
 }
 
+
 M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
