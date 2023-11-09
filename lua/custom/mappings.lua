4cffbbc26d42098674d930c01303b93c746e0fd8
Antonin Bonte <antonin.bonte@quickfds.eu>
2023-11-09 21:54:54 +0100

    added windows and tabs mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,12 +2,29 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
-  v = {
-    [">"] = { ">gv", "indent"},
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+		-- window management
+		["<leader>sv"] = { "<C-w>v", "Split window vertically" },
+		["<leader>sh"] = { "<C-w>s", "Split window horizontally" },
+		["<leader>se"] = { "<C-w>=", "Make splits equal size" },
+		["<leader>sx"] = { "<cmd>close<CR>", "Close current split" },
+
+		-- tab management
+		["<leader>to"] = { "<cmd>tabnew<CR>", "Open new tab" },
+		["<leader>tx"] = { "<cmd>tabclose<CR>", "Close current tab" },
+		["<leader>tn"] = { "<cmd>tabn<CR>", "Go to next tab" },
+		["<leader>tp"] = { "<cmd>tabp<CR>", "Go to previous tab" },
+		["<leader>tf"] = { "<cmd>tabnew %<CR>", "Open current buffer in new tab" },
+
+    -- maximize split 
+		["<leader>sm"] = { "<cmd>MaximizerToggle<CR>", "Maximize/minimize a split" },
+	},
+
+	v = {
+		[">"] = { ">gv", "indent" },
+	},
 }
 
 -- more keybinds!
